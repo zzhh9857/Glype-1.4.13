bb7c118adcc48bcb24694e51f1c444ed99c3fb0e
hstp <6bxy0t4v@yopmail.com>
2015-04-22 01:37:43 +0700

    Update theme

---
+++ themes/glacier/config.php
@@ -0,0 +1,54 @@
+<?php
+
+// Website name
+$themeReplace['site_name'] = 'Proxie - lets defeat Internet censorship';
+
+// Proxy meta description text
+$themeReplace['meta_description'] = <<<OUT
+Unblock blocked sites at school, at work or on a public network using our zero-configuration proxy technology. No ads or other nonsense. It's free and just works.
+OUT;
+
+// Proxy logo and slogan
+$themeReplace['logo'] = <<<OUT
+<h1>ProxieThemes</h1><h2><strong class="one"></strong>free <span>&amp;</span> secure<strong class="two"></strong></h2>
+OUT;
+
+// Proxy description - above form.
+$themeReplace['description'] = <<<OUT
+<strong>Unblock</strong> sites at <strong>school</strong>, at <strong>work</strong> or on a <strong>public network</strong> using our zero-configuration <strong>proxy</strong> technology, lets defeat <strong>Internet censorship</strong>.<strong></p>
+OUT;
+
+// URL Address inside form.
+$themeReplace['url'] = <<<OUT
+http://www.
+OUT;
+
+// Social Link Twitter
+$themeReplace['socialtwitter'] = <<<OUT
+https://www.twitter.com
+OUT;
+
+// Social Link Facebook
+$themeReplace['socialfacebook'] = <<<OUT
+https://www.facebook.com
+OUT;
+
+// Google analytics on all proxied pages.
+$themeReplace['google'] = <<<OUT
+
+
+OUT;
+
+// Put ads on all proxied pages.
+$themeReplace['proxied'] = <<<OUT
+
+
+OUT;
+
+// Partner websites located at footer.
+$themeReplace['partners'] = <<<OUT
+<span class="link">
+Partner links here.
+</span>
+
+OUT;
---
+++ themes/glacier/main.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title><!--[site_name]--></title>
+<link href="themes/glacier/tools/style.css" rel="stylesheet" type="text/css" />
+<meta name="robots" content="follow, all" />
+<meta name="googlebot" content="index, follow" />
+<meta name="revisit-after" content="10 days" />
+<meta name="description" content="<!--[site_name]-->">
+<link rel="icon" href="themes/glacier/images/fav.ico" type="image/x-icon">
+<script type="text/javascript" src="themes/glacier/tools/jquery.min.js"></script> 
+<script type="text/javascript" src="themes/glacier/tools/cufon-yui.js"></script>
+<script type="text/javascript" src="themes/glacier/tools/Abraham_Lincoln_400.font.js"></script>
+<script type="text/javascript" src="themes/glacier/tools/Inspiration_400.font.js"></script>
+<script type="text/javascript" src="themes/glacier/tools/Museo_Slab_100_400-Museo_Slab_700_400.font.js"></script>
+<script type="text/javascript">
+	Cufon.replace('.logo h1', {fontFamily: 'Inspiration'});
+	Cufon.replace('.logo h2', {fontFamily: 'Museo Slab 100'});
+	Cufon.replace('.logo h2 span', {fontFamily: 'Abraham Lincoln'});
+	Cufon.replace('p.big_text, p.small_text', {fontFamily: 'Museo Slab 100'});
+	Cufon.replace('p.big_text strong, p.small_text strong', {fontFamily: 'Museo Slab 700'});
+</script>
+</head>
+<body>
+<div id="transy">
+</div>
+<div id="wrapper">
+	<div class="logo"><!--[logo]--></div>
+	<div class="content">
+		<p class="big_text">
+		
+		<!--[description]-->
+	
+		<form action="includes/process.php?action=update" method="post" onsubmit="return updateLocation(this);" class="form">
+			<div class="field_content">
+		<input type="text" name="u" id="input" class="field" value="<!--[url]-->">
+		<input type="submit" value="" class="submit">
+			</div>
+		</form>
+		
+		<div class="clear"></div>
+		<div id="options">
+		<?php foreach($toShow as $details) { 
+				echo <<<OUT
+					<input type="checkbox" name="{$details['name']}" id="{$details['name']}"{$details['checked']} />
+					<label for="{$details['name']}">{$details['title']}</label>
+
+OUT;
+}
+?>
+		</div>
+		<ul class="social">
+			<li class="twitter"><a href="<!--[socialtwitter]-->"></a></li>
+			<li class="facebook"><a href="<!--[socialfacebook]-->"></a></li>
+		</ul>
+		<p><!--[error]--></p>
+		<div id="footer">
+		
+		<span class="link"><a href="http://www.glype.com/">Glype</a></span> | <span class="link"><a href="http://www.proxiethemes.com">Proxie Theme</a></span> | <!--[partners]-->
+		</div>
+	</div>
+</div>
+</body>
+</html>
